6ab145a243266178df92bb12fcf7b26c577c9b99
secuflag <secuflag@ticino.com>
2016-08-26 15:50:49 +0200

    Do not copy default hosts file

--- target/product/embedded.mk
+++ target/product/embedded.mk
@@ -94,5 +94,4 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     system/core/rootdir/init.usb.rc:root/init.usb.rc \
     system/core/rootdir/init.usb.configfs.rc:root/init.usb.configfs.rc \
-    system/core/rootdir/ueventd.rc:root/ueventd.rc \
-    system/core/rootdir/etc/hosts:system/etc/hosts
+    system/core/rootdir/ueventd.rc:root/ueventd.rc
